1d2943efbff285d4b7a5837a5284883363106d0b
Summer-Jay <uwoljh@naver.com>
2022-02-27 16:01:48 +0900

    [master] Main.js 작업

---
+++ src/components/HomepageMain/Header.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const Header = (props) =>{
+    return(
+        <div className="main_header_wrapper">
+
+            
+        </div>
+
+    )
+    
+}
+export default Header;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,13 @@
 import React, { useEffect, useRef, useMemo } from "react";
 
+import Header from "./src/components/HomepageMain/Header.js"
+
 const Main = () =>{
 
-    
+    return(
+        <Header/>
+
+    )
+
 }
 export default Main;
