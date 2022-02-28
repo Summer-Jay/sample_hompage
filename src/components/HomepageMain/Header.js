b7ecbd385eb8ab4d6db1690e4ab525b2c78a73a8
Summer-Jay <uwoljh@naver.com>
2022-03-01 00:18:51 +0900

    [master] 헤더영역 러프하게 처리

--- src/components/HomepageMain/Header.js
+++ src/components/HomepageMain/Header.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 
-
-const Header = (props) =>{
-    return(
-        <div className="main_header_wrapper">
-            헤더
-            
-        </div>
-    )
-    
-}
+const Header = (props) => {
+  return (
+    <div className="main_header_wrapper">
+      <span className="header_description">헤더영역</span>
+    </div>
+  );
+};
 export default Header;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,8 +3,6 @@ import React, { useEffect, useRef, useMemo, Component } from "react";
 import Header from "../components/HomepageMain/Header";
 
 const Main = (props) => {
-  console.log("main.js");
-
   return <Header />;
 };
 export default Main;
