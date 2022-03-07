5276851e10cd497efc7a73fcbc1da204b7c6966e
Summer-Jay <uwoljh@naver.com>
2022-03-07 15:07:19 +0900

    [master]컴포넌트 분리

--- src/components/HomepageMain/MainSlider.js
+++ src/components/HomepageMain/MainSlider.js
@@ -7,61 +7,14 @@ import tulips from "styles/img/tulips.jpg";
 import arrowLeft from "styles/img/arrow_left.svg";
 import arrowRight from "styles/img/arrow_right.svg";
 
+import CommonType from "components/slide/CommonType";
 const MainSlider = (props) => {
   const [nowPosit, setNowPosit] = useState(0);
-
-  const moveImg = (direc) => {
-    if (direc === "left") {
-      setNowPosit(nowPosit - 1);
-    } else {
-      setNowPosit(nowPosit + 1);
-    }
-  };
-
-  useEffect(() => {
-    console.log("nowposit", nowPosit);
-    document.querySelector(".picture_list").style.transform = `translate(-${
-      600 * nowPosit
-    }px)`;
-  }, [nowPosit]);
 
   return (
     <>
       <div className="slider_descriptsion">common</div>
-      <div className="slider_wrapper">
-        <div className="slider_container">
-          <div className="picture_list">
-            <ul className="slide_bundle">
-              <li className="slide_item">
-                <img src={toast} alt="토스트" />
-              </li>
-              <li className="slide_item">
-                <img src={cheezeCat} alt="고앵이" />
-              </li>
-              <li className="slide_item">
-                <img src={tulips} alt="튤립" />
-              </li>
-              <li className="slide_item">
-                <img src={cuteDog} alt="강아지" />
-              </li>
-            </ul>
-          </div>
-          {nowPosit !== 0 && (
-            <img
-              className="arrow_left"
-              src={arrowLeft}
-              onClick={() => moveImg("left")}
-            />
-          )}
-          {nowPosit !== 3 && (
-            <img
-              className="arrow_right"
-              src={arrowRight}
-              onClick={() => moveImg("right")}
-            />
-          )}
-        </div>
-      </div>
+      <CommonType nowPosit={nowPosit} setNowPosit={setNowPosit} />
       <div className="slider_descriptsion">Slick Library</div>
     </>
   );
---
+++ src/components/slide/CommonType.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import cheezeCat from "styles/img/cheezeCat.jpg";
+import cuteDog from "styles/img/cuteDog.jpg";
+import toast from "styles/img/toast.jpg";
+import tulips from "styles/img/tulips.jpg";
+import arrowLeft from "styles/img/arrow_left.svg";
+import arrowRight from "styles/img/arrow_right.svg";
+
+const MainSlider = (props) => {
+  const { nowPosit, setNowPosit } = props;
+
+  const moveImg = (direc) => {
+    if (direc === "left") {
+      setNowPosit(nowPosit - 1);
+    } else {
+      setNowPosit(nowPosit + 1);
+    }
+  };
+
+  useEffect(() => {
+    console.log("nowposit", nowPosit);
+    document.querySelector(".picture_list").style.transform = `translate(-${
+      600 * nowPosit
+    }px)`;
+  }, [nowPosit]);
+
+  return (
+    <div className="slider_wrapper">
+      <div className="slider_container">
+        <div className="picture_list">
+          <ul className="slide_bundle">
+            <li className="slide_item">
+              <img src={toast} alt="토스트" />
+            </li>
+            <li className="slide_item">
+              <img src={cheezeCat} alt="고앵이" />
+            </li>
+            <li className="slide_item">
+              <img src={tulips} alt="튤립" />
+            </li>
+            <li className="slide_item">
+              <img src={cuteDog} alt="강아지" />
+            </li>
+          </ul>
+        </div>
+        {nowPosit !== 0 && (
+          <img
+            className="arrow_left"
+            src={arrowLeft}
+            onClick={() => moveImg("left")}
+          />
+        )}
+        {nowPosit !== 3 && (
+          <img
+            className="arrow_right"
+            src={arrowRight}
+            onClick={() => moveImg("right")}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+export default MainSlider;
