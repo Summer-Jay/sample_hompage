e332e0e0f33fade90d311d8046cffedb06aa64e7
Summer-Jay <uwoljh@naver.com>
2022-03-07 15:08:31 +0900

    [master] 불필요 import 제거

--- src/components/HomepageMain/MainSlider.js
+++ src/components/HomepageMain/MainSlider.js
@@ -1,12 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
 
-import cheezeCat from "styles/img/cheezeCat.jpg";
-import cuteDog from "styles/img/cuteDog.jpg";
-import toast from "styles/img/toast.jpg";
-import tulips from "styles/img/tulips.jpg";
-import arrowLeft from "styles/img/arrow_left.svg";
-import arrowRight from "styles/img/arrow_right.svg";
-
 import CommonType from "components/slide/CommonType";
 const MainSlider = (props) => {
   const [nowPosit, setNowPosit] = useState(0);
